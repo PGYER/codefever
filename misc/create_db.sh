876323d6bdbb81621a2c803a72acf966ac9cc715
cubic <carneywu@pgyer.com>
2022-01-10 18:24:38 +0800

    fix(Deploy script): change script

--- misc/create_db.sh
+++ misc/create_db.sh
@@ -4,11 +4,11 @@ if [ $UID -ne 0 ]; then
     exit 1
 fi
 
-DB_HOST = `cat ../env.yaml | grep host | awk -F: '{ print $2 }'` | sed 's/^\s*//'
-DB_PORT = `cat ../env.yaml | grep port | awk -F: '{ print $2 }'` | sed 's/^\s*//'
-DB_USER = `cat ../env.yaml | grep username | awk -F: '{ print $2 }'` | sed 's/^\s*//'
-DB_PASS = `cat ../env.yaml | grep password | awk -F: '{ print $2 }'` | sed 's/^\s*//'
-DB_NAME = `cat ../env.yaml | grep db | awk -F: '{ print $2 }'` | sed 's/^\s*//'
+DB_HOST = `cat ../env.yaml | grep host: | awk -F: '{ print $2 }' | sed 's/^\s*//'`
+DB_PORT = `cat ../env.yaml | grep port: | awk -F: '{ print $2 }' | sed 's/^\s*//'`
+DB_USER = `cat ../env.yaml | grep username: | awk -F: '{ print $2 }' | sed 's/^\s*//'`
+DB_PASS = `cat ../env.yaml | grep password: | awk -F: '{ print $2 }' | sed 's/^\s*//'`
+DB_NAME = `cat ../env.yaml | grep db: | awk -F: '{ print $2 }' | sed 's/^\s*//'`
 
 mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p "$DB_PASS" -e "create database $DB_NAME";
 
